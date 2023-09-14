9ff8b0f84d7b6a9d944338d78e027e891252546e
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 03:36:08 +0100

    define home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import TopRated from "../components/TopRated";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <TopRated />
+    </>
+  );
+};
+
+export default Home;
