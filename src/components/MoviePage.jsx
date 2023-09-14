57be677438d62f8b76320dd098fb74bc32de6538
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 12:57:29 +0100

    Update file structure

---
+++ src/components/MoviePage.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/Sidebar";
+import MovieDetails from "../pages/MovieDetails";
+
+const MoviePage = () => {
+  return (
+    <section className="">
+      <Sidebar />
+      <MovieDetails />
+    </section>
+  );
+};
+
+export default MoviePage;
