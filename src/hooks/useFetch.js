9761c1026400fd98e05925592fb91199059d4bb1
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 03:35:08 +0100

    define a clear and custom fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+  //   const handleError = useErrorBoundary;
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        const movie = await res.json();
+        setLoading(false);
+        setData(movie);
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [setError, url]);
+  return { loading, data, error };
+}
