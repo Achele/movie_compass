d73813c9629344446ee589c4612fd6517dfa768f
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 03:31:35 +0100

    update movie details

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,31 @@
-const MovieCard = () => {
-  return <div></div>;
+import { Link } from "react-router-dom";
+import { IMDb, Tomatoe } from "./Icon";
+// import styles from "../styles/Hero.module.css";
+
+const MovieCard = (movies) => {
+  console.log(`results: ============= ${movies}`);
+  return (
+    <div>
+      <Link to={`/movies/${movies.id}`}>
+        <div className="border-2 rounded bg-black">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`}
+            alt="backdrop images"
+          />
+        </div>
+        <p>{movies.release_date}</p>
+        <h2>{movies.title}</h2>
+        <span>
+          <span>
+            <IMDb /> {movies.vote_average}
+          </span>
+          <span>
+            <Tomatoe /> {movies.popularity}
+          </span>
+        </span>
+      </Link>
+    </div>
+  );
 };
 
 export default MovieCard;
