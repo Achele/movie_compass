f77a7df404d207d712f49b037e62a74416eef87c
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 12:59:05 +0100

    Added footer component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import TopRated from "../components/TopRated";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Header />
       <TopRated />
+      <Footer />
     </>
   );
 };
