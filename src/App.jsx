e278ef0947df18f4dfafb9adcdb021de16583815
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-14 10:02:56 +0100

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import MoviePage from "./components/MoviePage";
 import Home from "./pages/Home";
-import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
       </Routes>
     </>
   );
