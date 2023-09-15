31a0c2ae61f5ab658eb31297cbec22f37380c61d
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-15 17:31:11 +0100

    Add the data-test attribute

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,16 +6,19 @@ const MovieCard = (movies) => {
   console.log(`results: ============= ${movies}`);
 
   return (
-    <div>
+    <div data-testid="movie-card" className="">
       <Link to={`/movies/${movies.id}`}>
         <div className="border-2 rounded bg-black">
           <img
+            data-testid="movie-poster"
             src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`}
             alt="backdrop images"
           />
         </div>
-        <p>{movies.release_date}</p>
-        <h2 className="font-bold">{movies.title}</h2>
+        <p data-testid="movie-release-date">{movies.release_date}</p>
+        <h2 data-testid="movie-title" className="font-bold">
+          {movies.title}
+        </h2>
         <span className="flex justify-between">
           <span className="flex items-center gap-1 text-xs">
             <IMDb /> {movies.vote_average?.toFixed(1) * 10}.0 / 100
