2f74fe3923fe62e8339ae3bb59417373db9cedad
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-15 15:56:51 +0100

    styling

--- src/components/TopRated.jsx
+++ src/components/TopRated.jsx
@@ -1,31 +1,16 @@
-// import { useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
 import { Loader } from "./Icon";
-// import styles from "../styles/Hero.module.css";
 import MovieCard from "./MovieCard";
 
 const TopRated = () => {
-  // const [toprated, setTopRated] = useState(null)
   const API_KEY = "805ad364617cc903792ab354ed226736";
   const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
   const { loading, data, error } = useFetch(url);
   const results = data?.results?.slice(0, 10);
-  // const [heroMovie, setHeroMovie] = useState(null);
-
-  // console.log(results);
-
-  // useEffect(() => {
-  //   if (results && results.length > 0) {
-  //     // Select the first movie as the hero movie
-  //     setHeroMovie(results[0]);
-  //   }
-  // }, [results]);
-
-  // console.log("Hero:", heroMovie);
 
   return (
     <section className="py-4 px-8">
-      <h1>Featured Movie</h1>
+      <h1 className="py-4 text-xl font-bold">Featured Movie</h1>
       {loading && <Loader />}
       {error && (
         <div className="error">{`There is a problem fetching the movie data - ${error}`}</div>
