0b58f8dc98e0c003ad4f82fa1ec3ff7972489bc7
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-09-15 15:49:03 +0100

    Add personalized configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      screens: {
+        xs: "279px",
+        // => @media (min-width: 279px) {...}
+
+        sm: "370px",
+        // => @media (min-width: 375px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
   },
   plugins: [],
 };
